85bb3f69b5cd3c1804ac35d97985eb3350b2ff39
desko27 <desko27@gmail.com>
2019-01-02 13:08:12 +0100

    Add margin between items in ListScreen

--- src/containers/screens/ListScreen.styles.js
+++ src/containers/screens/ListScreen.styles.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
+const cardMargin = 20;
+
 export const IdeaList = styled.div`
   display: flex;
+  flex-wrap: wrap;
+
+  margin: -${cardMargin}px;
+
+  > * {
+    margin: ${cardMargin}px;
+  }
 `;
